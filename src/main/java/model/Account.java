4d344568a1d22d8f8cbf4632a24e760764164400
Seyyed Parsa Neshaei <spneshaei@gmail.com>
2020-04-10 23:40:43 +0430

    Account Properties

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,4 +1,14 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Account {
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private String firstName;
+    private String lastName;
+    private ArrayList<Coupon> coupons;
+    private ArrayList<Log> logs;
 }
