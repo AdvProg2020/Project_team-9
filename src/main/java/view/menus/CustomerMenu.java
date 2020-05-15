af561c07a90e0e7443876426e8c376e27ebc92c3
Armin DM <arminmaher@gmail.com>
2020-05-16 02:42:12 +0430

    CustomerMenu editing step 2

--- src/main/java/view/menus/CustomerMenu.java
+++ src/main/java/view/menus/CustomerMenu.java
@@ -1,6 +1,7 @@
 package view.menus;
 
 import controller.DataManager;
+import model.Comment;
 import model.Customer;
 import model.Product;
 import model.Score;
@@ -9,10 +10,12 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class CustomerMenu extends Menu {
+    Product currentProduct;
+
     public CustomerMenu(String name, Menu parentMenu) {
         super(name, parentMenu);
-        HashMap<Integer,Menu> subMenus = new HashMap<>();
-        subMenus.put(1, new Menu("view cart",this) {
+        HashMap<Integer, Menu> subMenus = new HashMap<>();
+        subMenus.put(1, new Menu("View cart", this) {
             @Override
             public void execute() {
                 viewCart();
@@ -21,50 +24,63 @@ public class CustomerMenu extends Menu {
             }
 
             @Override
-            protected void showHelp(){
+            protected void showHelp() {
             }
         });
-    }
+        subMenus.put(2, new Menu("Show Product", this) {
+            @Override
+            public void execute() {
+                showProduct();
+            }
 
+            @Override
+            protected void showHelp() {
+
+            }
+        });
+        subMenus.put(3, new Menu("Increment Product's Number In Cart", this) {
+            @Override
+            public void execute() {
+                incrementProductCountInCart();
+            }
+
+            @Override
+            protected void showHelp() {
+
+            }
+        })
+    }
 
     private void viewCart() {
         Customer customer = (Customer) DataManager.shared().getLoggedInAccount();
         for (Product product : customer.getCart()) {
-            if (customer.getCart() == null){
+            if (customer.getCart() == null) {
                 System.out.println("There is no product in your cart yet");
                 break;
             }
             int counter = 0;
             counter++;
-            System.out.println(counter +". "+product);
+            System.out.println(counter + ". " + product);
         }
     }
 
-    private void showProduct(Product product) {
-        System.out.println("Product #" + product.getProductId());
-        System.out.println("Name: " + product.getName());
-        System.out.println("Brand: " + product.getBrand());
-        System.out.println("Price: " + product.getPrice());
-        System.out.println("Discount percent: " + product.getDiscountPercent());
-        System.out.println("Status: " + product.getStatus().toString());
-        System.out.println("Number available: " + product.getNumberAvailable());
+    private void showProduct() {
+        System.out.println("Enter the product id you want to show:");
+        int id = scanner.nextInt();
+        currentProduct = DataManager.shared().productWithID(id);
+        if (currentProduct == null) {
+            System.out.println("No product exists with the given ID");
+            return;
+        }
+        new ProductDetailsMenu("ProductDetailsMenu", parentMenu, currentProduct);
     }
 
-    private void startSubmittingScoreForProduct(Product product) {
-        System.out.println("Please enter your score to this product :");
-        int newScore =scanner.nextInt();
-        // TODO : check regex
-        Score score = new Score(UUID.randomUUID().toString(),(Customer)DataManager.shared().getLoggedInAccount(),newScore);
-        product.addScore(score);
+
+    private void incrementProductCountInCart() {
+
     }
 
-    private void startSubmittingCommentForProduct(Product product) {
-    }
-
-    private void incrementProductCountInCart(Product product) {
-    }
-
-    private void decrementProductCountInCart(Product product) {
+    private void decrementProductCountInCart() {
     }
 
     private void viewPurchaseLog() {
