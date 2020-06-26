e08cd0fd18864c875435ab8f9c116503caa6e9ee
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2020-06-26 23:44:49 +0430

    Further bug fixes

---
+++ app/src/main/java/com/sasp/saspstore/ui/VideoActivity.java
@@ -0,0 +1,16 @@
+package com.sasp.saspstore.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sasp.saspstore.R;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
